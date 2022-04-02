feded2a649e454e8ac002a0310b4b3a289061c85
David Godwin <david.godwin@sky.uk>
2022-04-02 12:56:56 +0100

    parsing single player from JSON response into player struct

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module play-if-needed
 
 go 1.17
+
+require github.com/yalp/jsonpath v0.0.0-20180802001716-5cc68e5049a0
--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -12,16 +13,30 @@ func main() {
 	log.Fatal(http.ListenAndServe(":9001", nil))
 }
 
+type Player struct {
+	Name string `json:"name"`
+	Rank int    `json:"rank"`
+}
+
 func Doubles(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "content-type")
 	w.Header().Set("Content-Type", "application/json")
 
-	json, err := ioutil.ReadAll(r.Body)
+	j, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		return
 	}
-	fmt.Println(string(json))
+
+	var data Player
+
+	err = json.Unmarshal(j, &data)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(data.Name)
+	fmt.Println(data.Rank)
 
 }
