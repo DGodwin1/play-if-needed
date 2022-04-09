091c7bb4fee0b5d03971443f9456477c8f3c50c2
David Godwin <david.godwin@sky.uk>
2022-04-09 14:30:49 +0100

    Start a basic a basic shuffle of players. Also starting to think about how we handle cases where someone puts in too many (or too few depending on how you look at it) people into the app and what to do in this case.

--- backend/main.go
+++ backend/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 )
 
@@ -37,8 +38,40 @@ func Doubles(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, v := range players {
-		fmt.Println(v.Name, v.Rank)
-	}
+	//for _, v := range players {
+	//	//fmt.Println(v.Name, v.Rank)
+	//}
 
+	remaining := len(players) % 4
+	if remaining > 1 {
+		//You have a difficult number of players to deal with. So let's handle it.
+		players, leftovers := makeThePairingsEasier(players, remaining)
+		fmt.Println(players)
+		fmt.Println(leftovers)
+		//do what we need to do with leftovers / players here. or at end?
+	}
+	//We have a nice number of players. Let's get to shuffling.
+
+	//Just a basic shuffle.
+	fmt.Printf("Got %v\n", players)
+	fmt.Println("TIME TO SHUFFLE")
+	rand.Shuffle(len(players), func(i, j int) { players[i], players[j] = players[j], players[i] })
+	fmt.Println(players)
+
+}
+
+func makeThePairingsEasier(players []Player, remaining int) ([]Player, []Player) {
+	//Based on a difficult number of players, will return a modified list of players in a normal doubles
+	//format and any 'leftovers' in a second Player array.
+	switch remaining {
+	case 1:
+		fmt.Println("Someone has to sit out. Sorry. Going to randomly pick someone.")
+		player := players[rand.Intn(len(players))]
+		fmt.Printf("%v is going to have to sit out.", player.Name)
+	case 2:
+		fmt.Println("A singles can take place. Picking two people.")
+	case 3:
+		fmt.Println("We need one more for another doubles or someone can sit out. 3 people to chat.")
+	}
+	return []Player{}, []Player{}
 }
